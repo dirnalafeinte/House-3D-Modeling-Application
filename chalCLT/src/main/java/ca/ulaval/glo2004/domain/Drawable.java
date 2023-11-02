11f282f8123c84823e13f75cc1550ac0811273c6
ZiyadBouazara <ziyad.bouazara1@ulaval.ca>
2023-11-02 17:54:24 -0400

    Started Drawable.calculeSommets()

--- chalCLT/src/main/java/ca/ulaval/glo2004/domain/Drawable.java
+++ chalCLT/src/main/java/ca/ulaval/glo2004/domain/Drawable.java
@@ -15,6 +15,7 @@ public abstract class Drawable {
         this.sommets = new HashMap<>();
         this.couleur = Color.BLACK;
         this.chalet = chalet;
+        calculateSommets(); // Remplir la hashmap avec les Vues et sommets
     }
 
     public UUID getId() {
@@ -41,25 +42,69 @@ public abstract class Drawable {
 //        this.sommets = sommets;
 //    } // do we want to be able to change the sommets ?
 
+    //briefly, this method is used to check if a point is inside a Drawable in the given Vue
     public boolean contient(Vue vue, Coordonnee coordonnee){
         return false;
     }
 
+    //briefly, this method is used to check if a Drawable is inside another Drawable in the given Vue
     public boolean contient(Drawable that){
         return false;
     }
 
+    //briefly,
     public Imperial getDistanceMinEntre(Drawable that){
         return null;
     }
 
     private void calculateSommets(){
 
-        for vue in Vue
-        //iterate through
+        if chalet.getToit().getSensDuToit() == Orientation.FACADE || chalet.getToit().getSensDuToit() == Orientation.ARRIERE{
+            sommets.put(Vue.PLAN, calculateSommetsPlan1());
+            sommets.put(Vue.FACADE, calculateSommetsFacade());
+            sommets.put(Vue.GAUCHE, calculateSommetsGauche());
+            sommets.put(Vue.ARRIERE, calculateSommetsArriere());
+            sommets.put(Vue.DROITE, calculateSommetsDroite());
+        }
+        else if chalet.getToit().getSensDuToit() == Orientation.DROITE || chalet.getToit().getSensDuToit() == Orientation.GAUCHE{
+            sommets.put(Vue.PLAN, calculateSommetsPlan2());
+            sommets.put(Vue.FACADE, calculateSommetsFacade());
+            sommets.put(Vue.GAUCHE, calculateSommetsGauche());
+            sommets.put(Vue.ARRIERE, calculateSommetsArriere());
+            sommets.put(Vue.DROITE, calculateSommetsDroite());
+        }
     }
 
+    //briefly, this method is used to calculate the sommets when the Vue is PLAN and the sensDuToit is FACADE or ARRIERE
+    private List<Coordonnee> calculateSommetsPlan1(){
+        new ArrayList<Coordonnee>();
 
+    }
 
+    //briefly, this method is used to calculate the sommets when the Vue is PLAN and the sensDuToit is DROITE or GAUCHE
+    private List<Coordonnee> calculateSommetsPlan2(){
+        new ArrayList<Coordonnee>();
 
+    }
+
+    private List<Coordonnee> calculateSommetsFacade(){
+        ArrayList<Coordonnee> myList = new ArrayList<Coordonnee>();
+        myList.add(new Coordonnee(0,0));
+        myList.add(new Coordonnee(chalet.getLongueur().toInt(), 0));
+        myList.add(new Coordonnee(0, chalet.getHauteur().toInt()));
+        myList.add(new Coordonnee(chalet.getLongueur().toInt(), chalet.getHauteur().toInt()));
+        return myList;
+    }
+
+    private List<Coordonnee> calculateSommetsGauche(){
+        new ArrayList<Coordonnee>();
+    }
+
+    private List<Coordonnee> calculateSommetsArriere(){
+        new ArrayList<Coordonnee>();
+    }
+
+    private List<Coordonnee> calculateSommetsDroite(){
+        new ArrayList<Coordonnee>();
+    }
 }
