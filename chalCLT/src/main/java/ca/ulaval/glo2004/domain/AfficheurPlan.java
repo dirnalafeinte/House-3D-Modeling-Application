9a7a07d1ee8142269781b2e64246a6d37703da13
Rihab Assabar <rihab.assabar.1@ulaval.ca>
2023-11-03 00:34:40 -0400

    Modification sommets vueDessus

--- chalCLT/src/main/java/ca/ulaval/glo2004/domain/AfficheurPlan.java
+++ chalCLT/src/main/java/ca/ulaval/glo2004/domain/AfficheurPlan.java
@@ -2,51 +2,51 @@ package ca.ulaval.glo2004.domain;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+import ca.ulaval.glo2004.domain.util.Coordonnee;
+import ca.ulaval.glo2004.domain.util.Imperial;
 
 
-public class AfficheurPlan extends Afficheur{
-    private Drawable chalet;
+
+public class AfficheurPlan extends Afficheur {
+    private Drawable drawable;
+    private Chalet chalet;
 
 
-    public AfficheurPlan(Drawable chalet){
+    public AfficheurPlan(Drawable chalet) {
         super();
-        this.chalet =  chalet;
+        this.chalet = chalet;
         String vue = chalet.getVue();
         drawPlan(g, vue);
     }
 
-    public void drawPlan(Graphics g) {
-          ArrayList<Coordonnee> sommetsPlan;
+    public void drawPlan(Graphics g) throws Exception {
+        ArrayList<Coordonnee> sommetsPlan;
 
-          if (chalet.getSensDuToit() == null) {
-              throw new Exception("L'orientation du toit doit être définie")
-          }
-
-          else if (chalet.getSensDuToit()== Orientation.FACADE || chalet.getSensDuToit() == Orientation.ARRIERE){
-              sommetsPlan = chalet.calculateSommetsPlan1();
-          }
-          else  {
-              sommetsPlan = chalet.calculateSommetsPlan2();
+        if (chalet.getSensDuToit() == null) {
+            throw new Exception("L'orientation du toit doit être définie");
+        } else if (chalet.getSensDuToit() == Orientation.FACADE || chalet.getSensDuToit() == Orientation.ARRIERE) {
+            sommetsPlan = chalet.calculateSommetsPlan1();
+        } else {
+            sommetsPlan = chalet.calculateSommetsPlan2();
 
 
+            for (int i = 0; i < 4; i++) {
+                Polygon poly = new Polygon();
 
+                for (int j = 0; j < 8; j++) {
+                    poly.addPoint(sommetsPlan.get(i + j).getX(), sommetsPlan.get(i + j).getY());
+                }
 
-       for (for i = 0; i< 4; i++) {
-           Polygon poly = new Polygon();
+                // Mur Façades
+                if (i == 0 || i == 2) {
+                    g.setColor(Color.ORANGE);
+                } else {
+                    g.setColor(Color.BLUE);
+                }
 
-           for (int j=0; j<8, j++){
-                poly.addPoint(sommetsPlan.get(i+j).getX(), coordonnee.Y());
-       }
+                g.fillPolygon(poly);
+
+            }
+        }
     }
-       // Mur Façades
-       if (i ==0 || i ==2) {
-           g.setColor(Color.ORANGE);
-       }
-       else{
-           g.setColor(Color.BLUE);
-       }
-
-       g.fillPolygon(poly);
-
 }
