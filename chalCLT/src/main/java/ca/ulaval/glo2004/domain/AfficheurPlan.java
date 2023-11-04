a6a2490d3534cb9385ed7674ad2ba14e30360f91
Rihab Assabar <rihab.assabar.1@ulaval.ca>
2023-11-04 15:38:22 -0400

    Modification sommets vueDessus

--- chalCLT/src/main/java/ca/ulaval/glo2004/domain/AfficheurPlan.java
+++ chalCLT/src/main/java/ca/ulaval/glo2004/domain/AfficheurPlan.java
@@ -2,37 +2,29 @@ package ca.ulaval.glo2004.domain;
 
 import java.awt.*;
 import java.util.*;
-import ca.ulaval.glo2004.domain.util.Coordonnee;
+
 import ca.ulaval.glo2004.domain.util.Imperial;
+import ca.ulaval.glo2004.domain.util.Coordonnee;
 
 
 
 
 public class AfficheurPlan extends Afficheur {
     private Chalet chalet;
-    private Drawable drawable;
 
-    public AfficheurPlan(Drawable drawable, Chalet chalet) {
-        super();
+    public AfficheurPlan(Chalet chalet) {
         this.chalet = chalet;
-        //String vue = drawable.getVue();
-        //drawPlan(Graphics g);
     }
 
 
     public void drawPlan(Graphics g){
 
-        Map<Vue, ArrayList<ArrayList<Imperial>>> sommetsPlan = drawable.getSommets();
-        ArrayList<ArrayList<Imperial>> coordonnees =  sommetsPlan.get(drawable.PLAN);
-
-
-        ArrayList<Imperial> xCoords = coordonnees.get(0);
-        ArrayList<Imperial>  yCoords = coordonnees.get(1);
+       List<Coordonnee> coordonnees = drawable.getSommets().get(Vue.PLAN);
 
 
             //On boucle pour les 4 murs
         for (int k = 0; k < 4; k++) {
-            Polygon poly = new Polygon(xCoords, yCoords, );
+            Polygon poly = new Polygon(xCoords, yCoords, sommets.size());
 
             //On boucle cette fois-ci sur chacun des sommets(8)
             for (int j = 0; j < 8; j++) {
