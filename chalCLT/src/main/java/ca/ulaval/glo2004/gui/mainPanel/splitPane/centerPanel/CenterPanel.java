27c18c26017e37755caa53f6082fff71d77702aa
Rihab Assabar <rihab.assabar.1@ulaval.ca>
2023-11-07 12:50:35 -0500

    Modification taille centerSplit

---
+++ chalCLT/src/main/java/ca/ulaval/glo2004/gui/mainPanel/splitPane/centerPanel/CenterPanel.java
@@ -0,0 +1,20 @@
+package ca.ulaval.glo2004.gui.mainPanel.splitPane.centerPanel;
+
+import ca.ulaval.glo2004.gui.MainWindow;
+
+import javax.swing.*;
+
+public class CenterPanel extends JPanel {
+    private final MainWindow mainWindow;
+    private final DrawingPanel drawingPanel;
+
+    public CenterPanel(MainWindow mainWindow) {
+        this.mainWindow = mainWindow;
+        drawingPanel = new DrawingPanel(mainWindow);
+        init();
+    }
+
+    private void init() {
+        add(drawingPanel);
+    }
+}
