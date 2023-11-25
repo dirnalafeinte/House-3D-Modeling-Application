e75f92f1a5826a7d95386cfc85eac26a551c9b6b
Nadir Berrezouk <naber259@ulaval.ca>
2023-11-24 20:43:45 -0500

    maj precision zoom

--- chalCLT/src/main/java/ca/ulaval/glo2004/gui/mouse/ZoomHandler.java
+++ chalCLT/src/main/java/ca/ulaval/glo2004/gui/mouse/ZoomHandler.java
@@ -2,10 +2,9 @@ package ca.ulaval.glo2004.gui.mouse;
 
 import ca.ulaval.glo2004.domain.drawers.Afficheur;
 import ca.ulaval.glo2004.gui.MainWindow;
-
-import java.awt.*;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
+import java.awt.geom.Point2D;
 
 public class ZoomHandler implements MouseWheelListener {
     private final MainWindow mainWindow;
@@ -19,16 +18,16 @@ public class ZoomHandler implements MouseWheelListener {
 
     @Override
     public void mouseWheelMoved(MouseWheelEvent e) {
-        int notches = e.getWheelRotation();
+        double notches = e.getPreciseWheelRotation();
         double mouseX = e.getX();
         double mouseY = e.getY();
 
         double scale = (notches < 0) ? 1.05 : 0.95;
         afficheur().setZoomFactor(afficheur().getZoomFactor()*scale);
 
-        Point originalMousePos = new Point(
-                (int)(mouseX / afficheur().getLastZoomFactor() - afficheur().getxOffset()),
-                (int)(mouseY / afficheur().getLastZoomFactor() - afficheur().getyOffset())
+        Point2D originalMousePos = new Point2D.Double(
+                (mouseX / afficheur().getLastZoomFactor() - afficheur().getxOffset()),
+                (mouseY / afficheur().getLastZoomFactor() - afficheur().getyOffset())
         );
 
         double newOffsetX = mouseX / afficheur().getZoomFactor() - originalMousePos.getX();
